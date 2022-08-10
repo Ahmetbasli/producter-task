9a9828ac69845219862d9da1001a1ac26aa55535
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-08-10 20:37:52 +0300

    Add importancy bar

---
+++ src/common/ImportancyBars/index.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import styles from "./index.module.scss";
+
+export interface ActiveBarNumber {
+  lower: number;
+  middle: number;
+  higher: number;
+}
+
+const ImportancyBarrs: FC<{ importancy: keyof ActiveBarNumber }> = ({
+  importancy,
+}) => {
+  const activeBarNumber = {
+    lower: 1,
+    middle: 2,
+    higher: 3,
+  };
+  const pickColor = () => {
+    if (activeBarNumber[importancy] === 1) {
+      return styles.green;
+    } else if (activeBarNumber[importancy] === 2) {
+      return styles.yellow;
+    } else {
+      return styles.red;
+    }
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      {["6px", "9px", " 12px"].map((height, index) => (
+        <div
+          key={height}
+          style={{ height }}
+          className={`${styles.bar} ${
+            activeBarNumber[importancy] <= index ? "" : pickColor()
+          }`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ImportancyBarrs;
