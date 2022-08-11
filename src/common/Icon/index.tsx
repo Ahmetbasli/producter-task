8c87d297509ab46a5390ce58df36fe1300f4c6bd
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-08-11 14:36:22 +0300

    Try next Image tag

--- src/common/Icon/index.tsx
+++ src/common/Icon/index.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
 import React, { FC } from "react";
 
 const Icon: FC<{ name: string }> = ({ name }) => {
-  return <img src={`/icons/${name}.svg`} alt={`${name} icon`} />;
+  return (
+    <Image
+      width={24}
+      height={24}
+      src={`/icons/${name}.svg`}
+      alt={`${name} icon`}
+    />
+  );
 };
 
 export default Icon;
