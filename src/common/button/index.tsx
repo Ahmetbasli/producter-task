404d5a9ce89c54d01fcb914892037b70b11bf854
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-08-11 04:09:27 +0300

    Update button component to be more generic

--- src/common/button/index.tsx
+++ src/common/button/index.tsx
@@ -2,18 +2,24 @@ import React, { FC } from "react";
 import Icon from "src/common/Icon/index";
 import styles from "./index.module.scss";
 interface ButtonProps {
-  icon: string;
   text: string;
+  icon?: string;
+  iconMarginLeft?: string;
   isActive?: boolean;
 }
 
-const Button: FC<ButtonProps> = ({ icon, text, isActive = false }) => {
+const Button: FC<ButtonProps> = ({
+  icon,
+  text,
+  isActive = false,
+  iconMarginLeft,
+}) => {
   return (
     <button
       className={`${styles.wrapper} ${isActive ? styles.wrapperActive : ""} `}
     >
-      <Icon name={icon} />
-      <span>{text}</span>
+      {icon && <Icon name={icon} />}
+      <span style={{ marginLeft: `${iconMarginLeft}` }}>{text}</span>
     </button>
   );
 };
