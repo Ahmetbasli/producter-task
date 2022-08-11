3d7f5c532363f2415cf2229a5dec2b8f7a91b000
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-08-11 16:39:19 +0300

    Add subheader

--- src/components/SubHeader/index.tsx
+++ src/components/SubHeader/index.tsx
@@ -1,8 +1,43 @@
 import React from "react";
+import Icon from "src/common/Icon";
 import styles from "./index.module.scss";
 
 const SubHeader = () => {
-  return <div className={styles.wrapper}>SubHeader</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.left}>
+        <button className={styles.arrow}>
+          <Icon name="arrow-left" />
+        </button>
+        <button className={styles.code}>
+          <Icon name="spr" />
+          <span>SPR-06</span>
+        </button>
+        <button className={styles.code}>
+          <Icon name="people" />
+          <span>TEAM-10</span>
+        </button>
+        <div className={styles.last}>
+          <span>New Sprint Name</span>
+          <button className={styles.calendar}>
+            <Icon name="calendar" />
+          </button>
+          <button className={styles.peopleGray}>
+            <Icon name="people-gray" />
+          </button>
+        </div>
+        <button className={styles.button}>Complete Sprint</button>
+      </div>
+      <div className={styles.right}>
+        <button className={styles.dashboardGray}>
+          <Icon name="dashboard-gray" />
+        </button>
+        <button>
+          <Icon name="white-rects" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SubHeader;
