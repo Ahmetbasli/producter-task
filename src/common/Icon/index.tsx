b802b9ac1ac2469b9c0d3ef618b22e55f55203f9
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-08-12 23:03:40 +0300

    Add img placeholder

--- src/common/Icon/index.tsx
+++ src/common/Icon/index.tsx
@@ -3,19 +3,23 @@ import React, { FC, useEffect, useRef, useState } from "react";
 
 const Icon: FC<{ name: string }> = ({ name }) => {
   const [isImageLoaded, setsImageLoaded] = useState(false);
+  const image = useRef<HTMLImageElement | null>(null);
   console.log(isImageLoaded);
   const handleLoad = () => {
     setsImageLoaded(true);
   };
-
+  useEffect(() => {
+    if (image.current!.complete) setsImageLoaded(true);
+  }, []);
   return (
     <>
-      {!isImageLoaded && <Skeleton variant="circular" width={16} height={16} />}
       <img
+        ref={image}
         src={`/icons/${name}.svg`}
         onLoad={handleLoad}
         alt={`${name} icon`}
       />
+      {!isImageLoaded && <Skeleton variant="circular" width={16} height={16} />}
     </>
   );
 };
