98cf502df860b859f63a4a5c59f18172c89a8211
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-08-12 22:05:51 +0300

    HandleImage load

--- src/common/Icon/index.tsx
+++ src/common/Icon/index.tsx
@@ -1,7 +1,23 @@
-import React, { FC } from "react";
+import { Skeleton } from "@mui/material";
+import React, { FC, useEffect, useRef, useState } from "react";
 
 const Icon: FC<{ name: string }> = ({ name }) => {
-  return <img src={`/icons/${name}.svg`} alt={`${name} icon`} />;
+  const [isImageLoaded, setsImageLoaded] = useState(false);
+  console.log(isImageLoaded);
+  const handleLoad = () => {
+    setsImageLoaded(true);
+  };
+
+  return (
+    <>
+      {!isImageLoaded && <Skeleton variant="circular" width={16} height={16} />}
+      <img
+        src={`/icons/${name}.svg`}
+        onLoad={handleLoad}
+        alt={`${name} icon`}
+      />
+    </>
+  );
 };
 
 export default Icon;
