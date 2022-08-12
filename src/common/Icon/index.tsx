5d15ae45a85b71e043590d3278e1bc115cd44b7b
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-08-12 15:53:37 +0300

    remove cache for file naming conflict

--- src/common/Icon/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react";
-
-const Icon: FC<{ name: string }> = ({ name }) => {
-  return <img src={`/icons/${name}.svg`} alt={`${name} icon`} />;
-};
-
-export default Icon;
--- src/components/KanbanBoard/Card/TaskList/types.ts
+++
@@ -1,6 +0,0 @@
-import { Task } from "src/store/features/board/types";
-
-export default interface TaskListProps {
-  TaskListId: number;
-  tasks: Task[];
-}
